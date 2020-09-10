7cb8af343d8475c130ec10cb23e82edb00491603
Edwin Moradian <edwin.moradian90@gmail.com>
2020-09-10 21:32:26 +0400

    added styled-components and started on navbar style

--- frontend/src/components/Navbar/NavbarView.jsx
+++ frontend/src/components/Navbar/NavbarView.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const NavbarList = styled.ul`
+    display: flex;
+    justify-content: space-around;
+    list-style: none;
+`
 
 const NavbarView = () => {
     return (
         <div className="navbarView">
-            <ul className="navbar">
+            <NavbarList className="navbar">
                 <li className="navbarItem">Add Stat</li>
                 <li className="navbarItem">Track.it</li>
                 <li className="navbarItem">Your progress</li>
                 <li className="navbarItem">More</li>
-            </ul>
+            </NavbarList>
         </div>
     );
 };
