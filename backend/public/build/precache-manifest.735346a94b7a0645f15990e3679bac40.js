614f770ac37d51c7547bf9c989b00139f842fc34
Edwin Moradian <edwin.moradian90@gmail.com>
2020-09-25 15:47:28 +0400

    deployment

---
+++ backend/public/build/precache-manifest.735346a94b7a0645f15990e3679bac40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e0a8c36d433f8108bae1d24772c4643",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b219d9796eafcc1858b3",
+    "url": "/static/css/main.7bee0350.chunk.css"
+  },
+  {
+    "revision": "381de79ef71231960047",
+    "url": "/static/js/2.254c27f7.chunk.js"
+  },
+  {
+    "revision": "8bb757413c38ce564bf5d6ba97eb4669",
+    "url": "/static/js/2.254c27f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b219d9796eafcc1858b3",
+    "url": "/static/js/main.e5e2d047.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
